32da64ad4baa561dd3de80c9d77fc31154eb5b49
Timur Usmanov <fonuatimur@gmail.com>
2024-06-19 12:35:24 +0700

    fix XPATH, add time delay on tab switch

--- pages/sbis_page.py
+++ pages/sbis_page.py
@@ -1,9 +1,10 @@
 from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
+import time
 
 class SbisPage(BasePage):
     CONTACTS_LINK = (By.LINK_TEXT, "Контакты")
-    TENSOR_BANNER = (By.XPATH, "//html/body/div[1]/div/div/div[2]/div[1]/div[2]/div[1]/div/div/div[1]/div/div[4]/div[1]/div/div/div[2]/div/a")
+    TENSOR_BANNER = (By.XPATH, '//*[@id="contacts_clients"]/div[1]/div/div/div[2]/div/a/img')
 
     def go_to_contacts(self):
         self.click_element(*self.CONTACTS_LINK)
@@ -13,5 +14,6 @@ class SbisPage(BasePage):
         self.switch_to_new_tab()
 
     def switch_to_new_tab(self):
+        time.sleep(2)
         new_tab = self.browser.window_handles[-1]
         self.browser.switch_to.window(new_tab)
