31a9dede6fa57342709911e589b60c1353d14cad
Timur Usmanov <fonuatimur@gmail.com>
2024-06-19 12:34:07 +0700

    add element present check

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage:
     def __init__(self, browser):
@@ -17,11 +18,12 @@ class BasePage:
 
     def click_element(self, *locator):
         element = self.find_element(*locator)
+        if not element:
+            raise NoSuchElementException
+        
         self.scroll_to_element(*locator)
         WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable(locator))
         self.browser.execute_script("arguments[0].click();", element)
-
-
 
     def wait_for_element(self, *locator, timeout=10):
         WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located(locator))
